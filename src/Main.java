0d8464f74f2d68e11f8559c2617afeff0dba7874
BSVJAVA <seregabbv@mail.ru>
2025-02-27 09:58:29 +0700

    HomeworkLesson_09 (Домашняя работа №9)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Задача №1
+        System.out.println("Задача №1");
+        String firstName = "Ivan";       //для хранения имени.
+        String middleName = "Ivanovich"; //для хранения отчества.
+        String lastName = "Ivanov";      //для хранения фамилии.
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //Задача №2
+        System.out.println("Задача №2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //Задача №3
+        System.out.println("Задача №3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName.replace('ё', 'е'));
     }
 }
